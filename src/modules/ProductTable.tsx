ebdbccc3ea931358dd192a3a74cb9b44efd56209
abdulhadiy-1 <hadiyhadiy2008@gmail.com>
2025-07-23 23:07:46 +0500

    loading

--- src/modules/ProductTable.tsx
+++ src/modules/ProductTable.tsx
@@ -73,9 +73,9 @@ const ProductTable = ({
       </div>
 
       {isLoading ? (
-        <div className="w-[100px] mx-auto text-2xl font-bold mt-[100px]">
-          Loading...
-        </div>
+      <div className="flex justify-center items-center h-40">
+        <div className="animate-spin rounded-full h-12 w-12 border-t-2 border-b-2 border-blue-500"></div>
+      </div>
       ) : data?.data.length ? (
         <div className="space-y-[8px]">
           {data?.data?.map((item) => (
